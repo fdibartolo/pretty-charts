aa1122b3a364da3e17de7a920df26251c89b0366
Fernando Di Bartolo <fernando.di.bartolo@gmail.com>
2020-10-11 19:25:12 -0300

    layout drilldown approach, wip

--- src/app/drilldown/drilldown.component.ts
+++ src/app/drilldown/drilldown.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ChartDataSets, ChartOptions } from 'chart.js';
+import { Label } from 'ng2-charts';
 
 @Component({
   selector: 'app-drilldown',
@@ -7,5 +9,30 @@ import { Component } from '@angular/core';
 })
 export class DrilldownComponent {
 
+  chartType = 'bar';
+  chartLabels: Label[ ] = [
+    'Containers',
+    'Development',
+    'Infra as Code',
+    'Orchestrators',
+    'Scripting',
+    'Source Control'
+  ];
 
+  chartData: ChartDataSets[] = [
+    { data: [6, 9, 8, 1, 5, 3], label: 'Familiarizado' },
+    { data: [5, 5, 1, 8, 6, 4], label: 'Usado' },
+    { data: [2, 4, 4, 9, 6, 5], label: 'Experto' }
+  ];
+
+  chartOptions: ChartOptions = {
+    responsive: true,
+    scales: { 
+      xAxes: [{
+        stacked: true,
+        gridLines: { display: false }
+      }], 
+      yAxes: [{ stacked: true, type: 'linear' }]
+    }
+  };
 }
