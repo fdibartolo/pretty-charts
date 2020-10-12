1e7c7bd12b1d289191acacc6dee58668fd0beb72
Fernando Di Bartolo <fernando.di.bartolo@gmail.com>
2020-10-12 20:26:02 -0300

    reorganize chart and data

--- src/app/drilldown/drilldown.component.ts
+++ src/app/drilldown/drilldown.component.ts
@@ -8,22 +8,34 @@ import { Label } from 'ng2-charts';
   styleUrls: ['./drilldown.component.css']
 })
 export class DrilldownComponent {
-
   chartType = 'bar';
-  chartLabels: Label[ ] = [
-    'Containers',
-    'Development',
-    'Infra as Code',
-    'Orchestrators',
-    'Scripting',
-    'Source Control'
+  chartLabels: Label[][] = [[
+    'Docker',
+    'Openshift',
+    'Kubernetes',
+    'AWS',
+    'GCP',
+    'Azure'
+  ],[
+    'Ansible',
+    'CloudFormation',
+    'Chef',
+    'Terraform',
+    'Puppet'
+  ]
   ];
 
-  chartData: ChartDataSets[] = [
-    { data: [6, 9, 8, 1, 5, 3], label: 'Familiarizado' },
-    { data: [5, 5, 1, 8, 6, 4], label: 'Usado' },
-    { data: [2, 4, 4, 9, 6, 5], label: 'Experto' }
-  ];
+  chartData: ChartDataSets[][] = [[
+    { data: [1, 2, 5, 8, 4, 2], label: 'Desconozco' },
+    { data: [6, 9, 8, 1, 5, 5], label: 'Familiarizado' },
+    { data: [5, 5, 1, 8, 6, 3], label: 'Usado' },
+    { data: [2, 4, 4, 9, 7, 7], label: 'Experto' }
+  ],[
+    { data: [2, 4, 4, 9, 7], label: 'Desconozco' },
+    { data: [1, 2, 5, 8, 4], label: 'Familiarizado' },
+    { data: [5, 5, 1, 8, 6], label: 'Usado' },
+    { data: [6, 9, 8, 1, 5], label: 'Experto'}
+  ]];
 
   chartOptions: ChartOptions = {
     responsive: true,
