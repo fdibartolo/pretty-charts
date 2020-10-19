ad478081cca178acd6a09dcf88f26997a041b3a5
Fernando Di Bartolo <fernando.di.bartolo@gmail.com>
2020-10-19 17:01:34 -0300

    add charts and sample data for tools analysis

--- src/app/tool/tool.component.ts
+++ src/app/tool/tool.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {Observable} from 'rxjs';
-import {debounceTime, distinctUntilChanged, map} from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { debounceTime, distinctUntilChanged, map } from 'rxjs/operators';
+import { ChartType, ChartOptions } from 'chart.js';
+import { Label } from 'ng2-charts';
 import ToolsData from '../../assets/data/tools.json';
 
 @Component({
@@ -18,4 +20,14 @@ export class ToolComponent {
       map(term => term.length < 2 ? []
         : ToolsData.tools.filter(v => v.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10))
     )
+
+  public pieChartOptions: ChartOptions = {
+    responsive: true,
+    legend: { position: 'top' }
+  };
+
+  pieChartLabels: Label[] = ToolsData.labels;
+  pieChartData: number[] = [300, 500, 100, 250];
+  pieChartType: ChartType = 'pie';
+  pieChartLegend = true;
 }
